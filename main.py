71f97e565c4eceed1c18a3c7480b8f844167da7a
Veronika Abanina <220390@mail.ru>
2022-10-05 13:35:26 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+# 1
+
+nested_list = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f', 'h', False],
+    [1, 2, None],
+]
+
+for list_ in nested_list:
+    for i in list_:
+        print(i)
+
+
+class FlatIterator:
+    def __init__(self, list_):
+        self.list_ = list_
+        self.cursor = -1
+        self.list_len = len(self.list_)
+
+    def __iter__(self):
+        self.cursor += 1
+        self.next = 0
+        return self
+
+    def __next__(self):
+        if self.next == len(self.list_[self.cursor]):
+            iter(self)
+        if self.cursor == self.list_len:
+            raise StopIteration
+        self.next += 1
+        return self.list_[self.cursor][self.next - 1]
+
+
+if __name__ == '__main__':
+    flat_list = [item for item in FlatIterator(nested_list)]
+    print(flat_list)
+
+
+# 2
+
+nested_list = [
+	['a', 'b', 'c'],
+	['d', 'e', 'f'],
+	[1, 2, None],
+]
+
+def flat_generator(nested_list):
+	for i in nested_list:
+		yield nested_list
+if __name__ == '__main__':
+	for item in flat_generator(nested_list):
+		print(item)
